3beb7ab1fb99df4175871c756bf82d7dda98f792
Durgesh Gupta <durgs7642@gmail.com>
2026-01-29 21:04:57 +0530

    final commit for Auth middleware

--- backend/Middlewares/AuthMiddlewares.js
+++ backend/Middlewares/AuthMiddlewares.js
@@ -1,23 +1,19 @@
 const User = require("../model/UsersModel");
-// require("dotenv").config();
+
 const jwt = require("jsonwebtoken");
 
 module.exports.userVerification = async (req, res, next) => {
+
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'unauthorized, JWT token is required' });
+  }
   try {
-    const token = req.cookies.token;
-    if (!token) return res.status(401).json({ status: false, message: "No token" });
-
-    jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
-      if (err) return res.json({ status: false });
-
-      const user = await User.findById(data.id).select("-password");
-      if (!user) return res.json({ status: false });
-
-      req.user = user;
-      next(); // ✅ continue to next route if verified
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ status: false, error: "Server error" });
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized, JWT token or expired", })
   }
 };
+

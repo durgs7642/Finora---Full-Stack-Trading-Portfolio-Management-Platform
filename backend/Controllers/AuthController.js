9e7011a829b72830ce58cf1c4ecd64712ae7ee40
Durgesh Gupta <durgs7642@gmail.com>
2026-02-07 19:37:28 +0530

    Final Commit

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -49,8 +49,8 @@ module.exports.Login = async (req, res)=> {
 
         res.cookie("token", token,{
             httpOnly: true,  // hide cookie from JS (good practice)
-            sameSite: "lax", // or 'none' if using https and cross-site
-            secure: false,  // set to true if using HTTPS
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // or 'none' if using https and cross-site
+            secure: process.env.NODE_ENV === "production",  // set to true if using HTTPS
             maxAge: 3 * 24 * 60 * 60 * 1000, 
         });
         res.status(200).json({message: "User logged in successfully", success: true});
@@ -63,8 +63,8 @@ module.exports.Login = async (req, res)=> {
 module.exports.Logout = async (req, res) => {
  await res.clearCookie("token", {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production",  // set to true if using HTTPS
     
   });
 
